7dac798ae45653d1aa96299f56d6388f3a8235d6
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-06-08 23:03:15 -0600

    V.1.0.0

--- pagina1.py
+++ pagina1.py
@@ -1,8 +1,17 @@
+###################################################
+#         Control de eventos sociales             #
+# V.0.0.0 //06 06 2025//                          #
+# V.1.0.0 //08 06 2025//                          #
+# Desplegado con streamlit                        #
+# Desarrollador: Sergio Emiliano López Bautista   #
+###################################################
+
 import streamlit as st
 
-st.set_page_config(page_title="Generador de diccionario", layout="wide")
-
+st.set_page_config(
+    page_title="Diana y Emiliano", 
+    layout="wide"
+)
 
 st.audio("luces.mp3", format="audio/mpeg", loop=True, autoplay=False)
-
 st.image("invitacion.png", caption="prueba 1")
