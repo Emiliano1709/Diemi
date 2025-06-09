077c989bdd9e9684c15462d84bf42c8f5410a097
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-06-09 16:18:21 -0600

    V.1.1.0

--- pagina1.py
+++ pagina1.py
@@ -6,13 +6,52 @@
 # Desarrollador: Sergio Emiliano López Bautista   #
 ###################################################
 
-import streamlit as st
 
+#--------------------- Requerimientos ---------------------
+import os
+import codecs
+import streamlit as st
+from streamlit.components.v1 import html
+from dotenv import load_dotenv, find_dotenv
+
+#---------------------- Estructuras -----------------------
+
+#----------------------- Funciones ------------------------
+
+#----------------------- Seteadores -----------------------
 st.set_page_config(
     page_title="Diana y Emiliano", 
-    layout="wide"
+    layout="wide",
+    page_icon = "💍"
 )
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path, override=True)
+GOOGLE_MAPS_API_KEY = os.getenv("DIEMI_MAPS_API_KEY")
 
+#----------------------- Interfaz -------------------------
 st.markdown("## ESTO ES SÓLO UNA PRUEBA. NO ES LA INVITACIÓN REAL")
-st.audio("luces.mp3", format="audio/mpeg", loop=True, autoplay=False)
+st.audio("luces.mp3", format = "audio/mpeg", loop = True, autoplay = True)
 st.image("invitacion.png", caption="prueba 1")
+
+
+#Dirección estática
+direccion = "Francisco Javier Mina 218, San Mateo, Tláhuac, 13040 Ciudad de México, CDMX, México"
+
+#Crear iframe del mapa
+mapa_html = f"""
+<iframe
+  width="100%"
+  height="500"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/place?key={GOOGLE_MAPS_API_KEY}&q={direccion.replace(' ', '+')}">
+</iframe>
+"""
+
+# Mostrar en la app
+st.write(f"Dirección mostrada: Salón Los Pinos")
+html(mapa_html, height=500)
+#------------------------ MAIN ----------------------------
+
